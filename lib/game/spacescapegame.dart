8f14b04d7b7a920beba827f4018c65a004646954
mkreatif <marlina.kreatif@gmail.com>
2022-01-30 21:03:55 +0700

    adding player move direction and joystic indicator

--- lib/game/spacescapegame.dart
+++ lib/game/spacescapegame.dart
@@ -1,19 +1,88 @@
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
 import 'package:flame/sprite.dart';
+import 'package:flutter/material.dart';
+import 'package:spacescape/game/player.dart';
 
-class SpacescapeGame extends FlameGame {
+class SpacescapeGame extends FlameGame with PanDetector {
+  late Player player;
+  Offset? _pointerStartPosition;
+  Offset? _pointerCurrentPosition;
+  final double _joysticRadius = 60, _deadZoneRadius = 10;
+
   @override
   Future<void>? onLoad() async {
     super.onLoad();
     await images.load('tilesheet.png');
     final spriteSheet = SpriteSheet.fromColumnsAndRows(
         image: images.fromCache('tilesheet.png'), columns: 8, rows: 6);
-    SpriteComponent player = SpriteComponent(
+    player = Player(
         sprite: spriteSheet.getSpriteById(6),
         size: Vector2(64, 64),
         position: canvasSize / 2);
+
     player.anchor = Anchor.center;
     add(player);
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    // render joystic
+    if (_pointerStartPosition != null) {
+      canvas.drawCircle(_pointerStartPosition!, _joysticRadius,
+          Paint()..color = Colors.grey.withAlpha(100));
+    }
+    // render joystic pointer
+    if (_pointerCurrentPosition != null) {
+      var delta = _pointerCurrentPosition! - _pointerStartPosition!;
+
+      if (delta.distance > _joysticRadius) {
+        Vector2 vektor = Vector2(delta.dx, delta.dy);
+        final normalize = vektor.normalized() * _joysticRadius;
+        delta = _pointerStartPosition! + normalize.toOffset();
+      } else {
+        delta = _pointerCurrentPosition!;
+      }
+      canvas.drawCircle(
+          delta, 20, Paint()..color = Colors.white.withAlpha(100));
+    }
+  }
+
+  @override
+  void onPanStart(DragStartInfo info) {
+    debugPrint('on pan start');
+    _pointerStartPosition = info.eventPosition.global.toOffset();
+    _pointerCurrentPosition = info.eventPosition.global.toOffset();
+  }
+
+  @override
+  void onPanUpdate(DragUpdateInfo info) {
+    debugPrint('on pan update');
+    _pointerCurrentPosition = info.eventPosition.global.toOffset();
+    final delta = _pointerCurrentPosition! - _pointerStartPosition!;
+    if (delta.distance > _deadZoneRadius) {
+      player.setMoveDirection(Vector2(delta.dx, delta.dy));
+    } else {
+      player.setMoveDirection(Vector2.zero());
+    }
+  }
+
+  @override
+  void onPanEnd(DragEndInfo info) {
+    debugPrint('on pan end');
+    _pointerStartPosition = null;
+    _pointerCurrentPosition = null;
+    player.setMoveDirection(Vector2.zero());
+  }
+
+  @override
+  void onPanCancel() {
+    debugPrint('on pan cancel');
+    _pointerStartPosition = null;
+    _pointerCurrentPosition = null;
+
+    player.setMoveDirection(Vector2.zero());
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:spacescape/game/spacescapegame.dart';
